bcdbeede7a6d8f88276d641c718d8d51a277351f
Thomas Lazarus <tlazarus@ksu.edu>
2020-06-26 22:57:46 -0500

    Begins working with spacy
    I think I'm going to take a little break from this project for a little bit.

--- abstractive.py
+++ abstractive.py
@@ -1,13 +1,18 @@
 import string
+from datetime import time
 
 import pandas as pd
 import re
+
+import spacy
+
+punctuation = string.punctuation + '\n' + '—' + '“' + ',' + '”' + '‘' + '-' + '’'
 
 
 def format_text(text):
     text = text.lower()
     text = re.sub('\[.*?\]', '', text)
-    text = re.sub('[%s]' % re.escape(string.punctuation), '', text)
+    text = re.sub('[%s]' % re.escape(punctuation), '', text)
     text = re.sub('\w*\d\w*', '', text)
     text = re.sub('[‘’“”…]', '', text)
     text = re.sub('\n', '', text)
@@ -24,6 +29,12 @@ del df['month']
 del df['year']
 
 df['content'] = df['content'].apply(format_text)
+df.drop(columns=['Unnamed: 0'], inplace=True)
+print(f"What's the shape: {df.shape}")
 
-for x in range(df.shape()[0]):
-    print(df.iloc[x])
+articles = []
+for i in range(df.shape[0]):
+    articles.append(df.iloc[i]['content'])
+
+
+nlp = spacy.load('en_core_web_sm', disable=['ner', 'parser'])
